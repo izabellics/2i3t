9b32ca046b7a615afcf1b6f5044eea6bac3b0e06
Francini <110108716+francielyy@users.noreply.github.com>
2023-10-09 09:47:57 -0300

    Add files via upload

--- 2i3t-main/script/script.js
+++ 2i3t-main/script/script.js
@@ -1,26 +1,95 @@
-escrever = msg => alert(msg);
-soma = (a,b) => a + b;
-sub = (a,b) => a - b;
-mult = (a,b) => a * b;
-div = (a,b) => a / b;
+let a = "";
+let b = "";
+let valor = "";
+let executar = "";
+let temPonto = false;
+let desligada = false;
+soma = (a,b) => Number(a) + Number(b);
+sub = (a,b) => Number(a) - Number(b);
+mult = (a,b) => Number(a) * Number(b);
+div = (a,b) => Number(a) / Number(b);
 raiz = a => Math.sqrt(a);
 equacao2Grau = (a,b,c) =>{
     let delta = sub(mult(b,b),mult(4,mult(a,c)));
-    if (delta<0) return "não possui raiz real.";
-    if (delta == 0)return div(-b,mult(2,a));
-    return "x1 = " + div(soma (-b,raiz(delta)), mult(2,a)) + "x2 = " + div(soma (b,raiz(delta)), mult(2,a));
-}
-let a = 0;
-let b = 0;
-let resultado = 0;
-let executar = "";
-
-calcular = () =>{
-    if(executar = "soma") mostrar_resultado (soma(a,b));
-    if(executar = "sub") mostrar_resultado (sub(a,b));
-    if(executar = "div") mostrar_resultado (div (a,b));
-    if(executar = "mult") mostrar_resultado (mult(a,b));
+    if (delta < 0) return "Não possui raiz Real.";
+    if (delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) + 
+           " x2 = " + div(sub(-b,raiz(delta)),mult(2,a));
 }
 
-//tentando... 1000  x 22/100 + 1000
-//escrever(equacao2Grau(3,-2,-1));
+function mostrar_resultado(){
+    document.getElementById("resultado").value = valor;
+}
+function calcular(){
+    if(desligada) return;
+    if(executar != ""){
+        b = valor;
+        if(executar == "soma") valor = soma(a,b);
+        if(executar == "sub") valor = sub(a,b);
+        if(executar == "div") valor = div(a,b);
+        if(executar == "mult") valor = mult(a,b);
+        if(executar == 'porc') valor = 0;
+        if(executar == "raiz") valor = raiz(a);
+        mostrar_resultado();
+        executar = "";
+        a = "";
+        b = "";
+        valor = "";
+        temPonto = false;
+    }
+}
+function desliga(){
+    desligada = !desligada;
+    console.log(desligada);
+    if(desligada){
+        zerar();
+    }else{
+        zerar();
+        mostrar_resultado();
+    }
+  return desligada;
+}
+
+function calcula_raiz(){
+    if(valor == ""){
+        valor = 0;
+    }
+    valor = raiz(valor);
+    mostrar_resultado();
+}
+function porcentagem(){
+   if(executar == "mult"){
+       b = valor;
+       valor = div(mult(a,b),100);
+       mostrar_resultado();
+       valor = "";
+   }
+}
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    valor = "0";
+    mostrar_resultado();
+    executar = "";
+    valor = "";
+}
+function operacao(op){
+    if(desligada) return;
+    executar = op;
+    a = valor;
+    valor = "";
+}
+function digitando(tecla){
+    if(desligada) return;
+   if (tecla == "."){
+       if(!temPonto) {
+         valor = valor + tecla;
+         mostrar_resultado();
+         temPonto = true;
+       }
+       return;
+   }
+   valor = valor + tecla;
+   mostrar_resultado();
+}
